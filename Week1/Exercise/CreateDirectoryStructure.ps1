79df8be2343f7fbd305c01cde8e544930f480268
Kasper P. B. Jensen <kasp734e@gmail.com>
2025-01-28 12:31:16 +0100

    Lavede uge 1 opgaver

---
+++ Week1/Exercise/CreateDirectoryStructure.ps1
@@ -0,0 +1,7 @@
+$WeekNo=$args[0]
+
+mkdir ./Week${WeekNo}
+mkdir ./Week${WeekNo}/Exercise
+touch ./Week${WeekNo}/Notes.txt
+touch ./Week${WeekNo}/Exercise/Exercise1.ps1
+touch ./Week${WeekNo}/Exercise/Exercise2.ps1
---
+++ Week1/Exercise/Exercise2.ps1
@@ -0,0 +1,4 @@
+Get-ChildItem -Recurse -File |
+Where-Object { $_.Length -gt 10MB } |
+Sort-Object Length
+ForEach-Object { Write-Output $_.FullName }
