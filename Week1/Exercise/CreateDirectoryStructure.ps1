f1e315fdd0c8eebc28801460a2f8216f58cbef7a
Kasper P. B. Jensen <kasp734e@gmail.com>
2025-01-28 12:39:10 +0100

    Lavede kommentarer i CreateDirectoryStructure.ps1

--- Week1/Exercise/CreateDirectoryStructure.ps1
+++ Week1/Exercise/CreateDirectoryStructure.ps1
@@ -1,7 +1,11 @@
+# Tager et argument som ugenummer
 $WeekNo=$args[0]
 
+# Opretter mappen Week${WeekNo} og undermappen Exercise
 mkdir ./Week${WeekNo}
 mkdir ./Week${WeekNo}/Exercise
+
+# Opretter filerne Notes.txt, Exercise1.ps1 og Exercise2.ps1
 touch ./Week${WeekNo}/Notes.txt
 touch ./Week${WeekNo}/Exercise/Exercise1.ps1
 touch ./Week${WeekNo}/Exercise/Exercise2.ps1
