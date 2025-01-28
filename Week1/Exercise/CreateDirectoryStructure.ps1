4f1f7405955eb98baa7b343d3bf96913b865874c
Kasper P. B. Jensen <kasp734e@gmail.com>
2025-01-28 13:06:18 +0100

    Lavet om til noget mere powershell agtigt

--- Week1/Exercise/CreateDirectoryStructure.ps1
+++ Week1/Exercise/CreateDirectoryStructure.ps1
@@ -1,11 +1,13 @@
 # Tager et argument som ugenummer
-$WeekNo=$args[0]
+param (
+    [int]$WeekNo
+)
 
-# Opretter mappen Week${WeekNo} og undermappen Exercise
-mkdir ./Week${WeekNo}
-mkdir ./Week${WeekNo}/Exercise
+# Opretter mappen Week$WeekNo og undermappen Exercise
+New-Item -ItemType Directory -Path "./Week$WeekNo"
+New-Item -ItemType Directory -Path "./Week$WeekNo/Exercise"
 
 # Opretter filerne Notes.txt, Exercise1.ps1 og Exercise2.ps1
-touch ./Week${WeekNo}/Notes.txt
-touch ./Week${WeekNo}/Exercise/Exercise1.ps1
-touch ./Week${WeekNo}/Exercise/Exercise2.ps1
+New-Item -ItemType File -Path "./Week$WeekNo/Notes.txt"
+New-Item -ItemType File -Path "./Week$WeekNo/Exercise/Exercise1.ps1"
+New-Item -ItemType File -Path "./Week$WeekNo/Exercise/Exercise2.ps1"
